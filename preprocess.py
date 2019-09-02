98906fe5808ab37853bf01f601b6e3dd387901e2
kozistr <kozistr@gmail.com>
2019-09-03 01:59:20 +0900

    update: preprocess code

--- preprocess.py
+++ preprocess.py
@@ -2,134 +2,101 @@ import argparse
 import os
 
 
-def preprocess_celeba(args):
-    if not os.path.exists(args.dest):
-        os.mkdir(args.dest)
+def preprocess_celeba(config):
+    if not os.path.exists(config.dest):
+        os.mkdir(config.dest)
 
-    allA = []
-    allB = []
+    all_a = []
+    all_b = []
 
-    with open(args.attributes) as f:
+    with open(config.attributes) as f:
         lines = f.readlines()
 
-    # Eyeglasses
-    if args.config == 'glasses':
-        for line in lines[2:]:
-            line = line.split()
-            if int(line[16]) == 1:
-                allA.append(line[0])
-            elif int(line[16]) == -1:
-                allB.append(line[0])
+    for line in lines[2:]:
+        line = line.split()
+        if int(line[config.custom]) == 1:
+            all_a.append(line[0])
+        else:
+            all_b.append(line[0])
 
-    # Mouth slightly open
-    if args.config == 'mouth':
-        for line in lines[2:]:
-            line = line.split()
-            if int(line[22]) == 1:
-                allA.append(line[0])
+    test_a = all_a[:config.num_test_imgs]
+    test_b = all_b[:config.num_test_imgs]
+    train_a = all_a[config.num_test_imgs:]
+    train_b = all_b[config.num_test_imgs:]
+
+    with open(os.path.join(config.dest, 'testA.txt'), 'w') as f:
+        for i, _img in enumerate(test_a):
+            if i == len(test_a) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                allB.append(line[0])
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    # Facial hair
-    if args.config == 'beard':
-        for line in lines[2:]:
-            line = line.split()
-            if int(line[21]) == 1 and int(line[1]) == -1 and (int(line[23]) == 1 or int(line[17]) == 1 or int(
-                    line[25]) == -1):  # male AND (mustache OR goatee OR beard) AND (no shadow)
-                allA.append(line[0])
-            elif int(line[21]) == 1 and int(line[25]) == 1 and int(line[1]) == -1:  # male AND (no beard, no shadow)
-                allB.append(line[0])
-
-    # Custom
-    if args.config == 'custom':
-        for line in lines[2:]:
-            line = line.split()
-            if int(line[args.custom]) == 1:
-                allA.append(line[0])
+    with open(os.path.join(config.dest, 'testB.txt'), 'w') as f:
+        for i, _img in enumerate(test_b):
+            if i == len(test_b) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                allB.append(line[0])
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    testA = allA[:args.num_test_imgs]
-    testB = allB[:args.num_test_imgs]
-    trainA = allA[args.num_test_imgs:]
-    trainB = allB[args.num_test_imgs:]
-
-    with open(os.path.join(args.dest, 'testA.txt'), 'w') as f:
-        for i, _img in enumerate(testA):
-            if i == len(testA) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+    with open(os.path.join(config.dest, 'trainA.txt'), 'w') as f:
+        for i, _img in enumerate(train_a):
+            if i == len(train_a) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    with open(os.path.join(args.dest, 'testB.txt'), 'w') as f:
-        for i, _img in enumerate(testB):
-            if i == len(testB) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+    with open(os.path.join(config.dest, 'trainB.txt'), 'w') as f:
+        for i, _img in enumerate(train_b):
+            if i == len(train_b) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    with open(os.path.join(args.dest, 'trainA.txt'), 'w') as f:
-        for i, _img in enumerate(trainA):
-            if i == len(trainA) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+
+def preprocess_folders(config):
+    if not os.path.exists(config.dest):
+        os.mkdir(config.dest)
+
+    train_a = os.listdir(os.path.join(config.root, 'trainA'))
+    train_b = os.listdir(os.path.join(config.root, 'trainB'))
+    test_a = os.listdir(os.path.join(config.root, 'testA'))
+    test_b = os.listdir(os.path.join(config.root, 'testB'))
+
+    with open(os.path.join(config.dest, 'testA.txt'), 'w') as f:
+        for i, _img in enumerate(test_a):
+            if i == len(test_a) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    with open(os.path.join(args.dest, 'trainB.txt'), 'w') as f:
-        for i, _img in enumerate(trainB):
-            if i == len(trainB) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+    with open(os.path.join(config.dest, 'testB.txt'), 'w') as f:
+        for i, _img in enumerate(test_b):
+            if i == len(test_b) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-
-def preprocess_folders(args):
-    if not os.path.exists(args.dest):
-        os.mkdir(args.dest)
-
-    trainA = os.listdir(os.path.join(args.root), 'trainA')
-    trainB = os.listdir(os.path.join(args.root), 'trainB')
-    testA = os.listdir(os.path.join(args.root), 'testA')
-    testB = os.listdir(os.path.join(args.root), 'testB')
-
-    with open(os.path.join(args.dest, 'testA.txt'), 'w') as f:
-        for i, _img in enumerate(testA):
-            if i == len(testA) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+    with open(os.path.join(config.dest, 'trainA.txt'), 'w') as f:
+        for i, _img in enumerate(train_a):
+            if i == len(train_a) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
-    with open(os.path.join(args.dest, 'testB.txt'), 'w') as f:
-        for i, _img in enumerate(testB):
-            if i == len(testB) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
+    with open(os.path.join(config.dest, 'trainB.txt'), 'w') as f:
+        for i, _img in enumerate(train_b):
+            if i == len(train_b) - 1:
+                f.write("%s" % os.path.join(config.root, _img))
             else:
-                f.write("%s\n" % os.path.join(args.root, _img))
-
-    with open(os.path.join(args.dest, 'trainA.txt'), 'w') as f:
-        for i, _img in enumerate(trainA):
-            if i == len(trainA) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
-            else:
-                f.write("%s\n" % os.path.join(args.root, _img))
-
-    with open(os.path.join(args.dest, 'trainB.txt'), 'w') as f:
-        for i, _img in enumerate(trainB):
-            if i == len(trainB) - 1:
-                f.write("%s" % os.path.join(args.root, _img))
-            else:
-                f.write("%s\n" % os.path.join(args.root, _img))
+                f.write("%s\n" % os.path.join(config.root, _img))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--root", default="",
-                        help="path to the celeba folder, or if you\'re using another dataset this should be the path to the root")
-    parser.add_argument("--dest", default="", help="path to the destination folder")
-    parser.add_argument("--attributes", default="", help="path to the attributes file")
+    parser.add_argument("--root", default="")
+    parser.add_argument("--dest", default="./bald", help="path to the destination folder")
     parser.add_argument("--num_test_imgs", default=64, help="number of images in the test set")
-    parser.add_argument("--config", default="glasses", help="configs available: glasses, mouth, beard")
-    parser.add_argument("--custom", default=32, help="use a custom celeba attribute")
+    parser.add_argument("--custom", default=5, help="use a custom celeba attribute")
     parser.add_argument("--folders", action="store_true",
                         help="use custom folders, instead of celeba")
 
